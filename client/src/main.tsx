b65dde25215238872b90839d2378f66ae691e9c6
Selvakumar B <b.selvakumar@gmail.com>
2026-01-27 02:26:16 +0530

    Deployment verification markers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+console.log("%c STORE-AI: ACTIVE BUILD V3.0 ", "background: #ef4444; color: white; font-weight: bold; padding: 4px;");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
