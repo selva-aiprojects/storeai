cb533220fc5bb0b68e0097be0bdf445a4851f529
Selvakumar B <b.selvakumar@gmail.com>
2026-02-02 23:33:37 +0530

    fix: definitive fix for malformed imports in main.py

--- python_backend/main.py
+++ python_backend/main.py
@@ -360,4 +360,4 @@ async def startup_event():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
